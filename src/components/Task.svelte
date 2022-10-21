957754e2cac1cc1a48427de9a1d9d1d025e96ab8
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-10-21 17:45:15 +0100

    Simple Component task updated

--- src/components/Task.svelte
+++ src/components/Task.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
   import { createEventDispatcher } from "svelte";
 
   const dispatch = createEventDispatcher();
@@ -30,4 +30,67 @@
   <label for="title" aria-label={task.title}>
     <input type="text" value={task.title} name="title" readonly />
   </label>
+</div> -->
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  // Event handler for Pin Task
+  function PinTask() {
+    dispatch("onPinTask", {
+      id: task.id,
+    });
+  }
+  // Event handler for Archive Task
+  function ArchiveTask() {
+    dispatch("onArchiveTask", {
+      id: task.id,
+    });
+  }
+
+  // Task props
+  export let task = {
+    id: "",
+    title: "",
+    state: "",
+  };
+
+  // Reactive declaration (computed prop in other frameworks)
+  $: isChecked = task.state === "TASK_ARCHIVED";
+</script>
+
+<div class="list-item {task.state}">
+  <label for="checked" class="checkbox" aria-label={`archiveTask-${task.id}`}>
+    <input
+      type="checkbox"
+      checked={isChecked}
+      disabled
+      name="checked"
+      id={`archiveTask-${task.id}`}
+    />
+    <span
+      class="checkbox-custom"
+      on:click={ArchiveTask}
+      aria-label={`archiveTask-${task.id}`}
+    />
+  </label>
+  <label for="title" aria-label={task.title} class="title">
+    <input
+      type="text"
+      value={task.title}
+      readonly
+      name="title"
+      placeholder="Input title"
+    />
+  </label>
+  {#if task.state !== "TASK_ARCHIVED"}
+    <button
+      class="pin-button"
+      on:click|preventDefault={PinTask}
+      id={`pinTask-${task.id}`}
+    >
+      <span class="icon-star" />
+    </button>
+  {/if}
 </div>
