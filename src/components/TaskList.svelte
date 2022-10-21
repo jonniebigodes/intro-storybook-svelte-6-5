12604de5924986b6f1d6a287f7a0903991a62133
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2022-10-21 18:08:40 +0100

    Composite component done

--- src/components/TaskList.svelte
+++ src/components/TaskList.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
   import Task from "./Task.svelte";
   export let loading = false;
   export let tasks = [];
@@ -15,5 +15,41 @@
   <div class="list-items">empty</div>
 {/if}
 {#each tasks as task}
+  <Task {task} on:onPinTask on:onArchiveTask />
+{/each} -->
+<script>
+  import Task from "./Task.svelte";
+  import LoadingRow from "./LoadingRow.svelte";
+  export let loading = false;
+  export let tasks = [];
+
+  //👇 Reactive declarations (computed props in other frameworks)
+  $: noTasks = tasks.length === 0;
+  $: emptyTasks = noTasks && !loading;
+  $: tasksInOrder = [
+    ...tasks.filter((t) => t.state === "TASK_PINNED"),
+    ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+  ];
+</script>
+
+{#if loading}
+  <div class="list-items">
+    <LoadingRow />
+    <LoadingRow />
+    <LoadingRow />
+    <LoadingRow />
+    <LoadingRow />
+  </div>
+{/if}
+{#if emptyTasks}
+  <div class="list-items">
+    <div class="wrapper-message">
+      <span class="icon-check" />
+      <p class="title-message">You have no tasks</p>
+      <p class="subtitle-message">Sit back and relax</p>
+    </div>
+  </div>
+{/if}
+{#each tasksInOrder as task}
   <Task {task} on:onPinTask on:onArchiveTask />
 {/each}
